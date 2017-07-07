c02df7ab4ce41a0b8822a0a69619c61827416fbf
zxftag <zhouxuefeng@live.cn>
2017-07-07 17:55:29 +0800

    Site updated: 2017-07-07 17:55:29

--- js/src/custom.js
+++ js/src/custom.js
@@ -1,5 +1,5 @@
 jQuery(function(){
     var subtitle = $(".site-subtitle").html();
-    var blink = "<span class=\"blink\">>_</span>";
+    var blink = "<span class=\"blink\">$</span>";
     $(".site-subtitle").html(subtitle.replace("$",blink));
 });
